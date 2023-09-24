58435441888e284026cb8e6773dfd3035c82162f
Ashkan <maryamsoltanebrahimi@gmail.com>
2023-09-25 00:25:06 +0330

    Add Download Modal

--- public/js/script.js
+++ public/js/script.js
@@ -1,5 +1,7 @@
 console.log('script loaded');
 const searchButton = document.querySelector('#searchButton');
+const downloadDialog = document.querySelector('#downloadDialog');
+
 searchButton.addEventListener('mouseover', () => {
     searchButton.textContent = "بزن بریم";
 });
@@ -7,3 +9,8 @@ searchButton.addEventListener('mouseout', () => {
     searchButton.innerHTML = '<i class="fa fa-search mr-5"></i';
     searchButton.innerHTML += "جستجو";
 });
+
+searchButton.addEventListener('click', () => {
+    downloadDialog.showModal();
+});
+
